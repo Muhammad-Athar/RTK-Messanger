8cb7ac624bf52dd18179d0c50e2c7b0d54dba2b9
Muhammad Athar <58026305+Muhammad-Athar@users.noreply.github.com>
2024-02-07 12:53:15 +0500

    Update Loader.jsx

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,14 +1,13 @@
 function Loader() {
     return (
-        <div className="flex items-center justify-center text-2xl">
-            <div className="animate-loading-dots">
-                <span className="text-[20px] animate-loading-dot1">.</span>
-                <span className="text-[20px] animate-loading-dot2">.</span>
-                <span className="text-[20px] animate-loading-dot3">.</span>
+        <div className="flex items-center justify-center text-2xl ">
+            <div className="animate-loading-dots flex space-x-2">
+                <span className="text-4xl bg-slate-700 h-5 w-5 rounded-full animate-loading-dot1"></span>
+                <span className="text-4xl bg-slate-700 h-5 w-5 rounded-full animate-loading-dot2"></span>
+                <span className="text-4xl bg-slate-700 h-5 w-5 rounded-full animate-loading-dot3"></span>
             </div>
         </div>
     );
 }
 
 export default Loader;
-
